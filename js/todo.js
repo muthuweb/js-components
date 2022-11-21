f14e5264eec649ffdd0251a2e3965f27ffc5b210
Muthukrishnan <muthukrishnan@luminescent.digital>
2022-11-21 15:37:08 +0530

    Improved js code

--- js/todo.js
+++ js/todo.js
@@ -36,83 +36,59 @@ function appendItem(value) {
   btnGroup.appendChild(btnDelete).innerHTML = '<i class="bi bi-trash"></i>';
 }
 
+const lsKey = "myItems";
+let todoItems = JSON.parse(localStorage.getItem(lsKey)) || [];
 
+// Parse the serialized data back into an array of objects
+localStorage.setItem(lsKey, JSON.stringify(todoItems));
 
-
- // Parse the serialized data back into an array of objects
-  let ddd = JSON.parse(localStorage.getItem("myItems")) || [];
+function storeItem(data) {
+  let newList = {
+    id: todoItems.length,
+    item: data,
+    done: false,
+  };
+  todoItems.push(newList);
 
   // Re-serialize the array back into a string and store it in localStorage
-  localStorage.setItem("myItems", JSON.stringify(ddd));
-
-
-
-
-
-
-function saveDataOnLocalStorage(data) {
-  // Parse the serialized data back into an array of objects
-  todoItems = JSON.parse(localStorage.getItem("toDoData")) || [];
-  // Push the new data (whether it be an object or anything else) onto the array
-  todoItems.push(data);
-  // Re-serialize the array back into a string and store it in localStorage
-  localStorage.setItem("toDoData", JSON.stringify(todoItems));
+  localStorage.setItem(lsKey, JSON.stringify(todoItems));
 }
-
-let todoItems = "";
-
-//Get items from local storage
-todoItems = JSON.parse(localStorage.getItem("toDoData"));
 
 toDoForm.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const formData = new FormData(toDoForm);
-
-  // const formDataURI = new URLSearchParams(formData).toString();
-  const formObject = Object.fromEntries(formData);
-  const formJSON = JSON.stringify(formObject);
-
-  /*   const xhr = new XMLHttpRequest();
-  xhr.open("GET", "https://reqres.in/api/users/2", true);
-  xhr.onload = function () {
-    let result = JSON.parse(xhr.responseText);
-    console.log(result.data);
-  };
-  xhr.send(); */
-
-  fetch("https://reqres.in/api/users/2", {
-    method: "GET",
-  })
-    .then((response) => response.json()) // get the json data from response object.
-    .then((result) => console.table(result.data)); // display data in console
-
   let value = todoInput.value;
   if (value) {
+    storeItem(value);
+    displayList(true);
     appendItem(value);
-    saveDataOnLocalStorage(value);
     todoInput.value = null;
   }
 });
 
 if (todoItems) {
-  todoItems.forEach((item) => appendItem(item));
+  todoItems.forEach((list) => appendItem(list.item));
 }
+
+function displayList(state) {
+  let listWrap = document.querySelector(".list-items");
+  listWrap.hidden = !state;
+}
+displayList(todoItems.length > 0);
 
 toListItem.addEventListener("click", function (event) {
   const deleteBtn = event.target.parentNode;
   if (deleteBtn && deleteBtn.classList.contains("btn-delete")) {
     removeItem(event);
+    displayList(todoItems.length > 0);
   }
 });
 
 function removeItem(event) {
-  //let parent = event.target.parentNode.parentNode.parentNode;
   let parent = event.target.closest(".list-group-item");
   let currentData = parent.firstChild.innerText;
   let index = todoItems.indexOf(currentData);
 
   parent.remove();
   todoItems.splice(index, 1);
-  localStorage.setItem("toDoData", JSON.stringify(todoItems));
+  localStorage.setItem(lsKey, JSON.stringify(todoItems));
 }
